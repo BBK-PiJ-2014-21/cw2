52756ab81aba77e0f5f27ef1208fd986a64aea99
fbarto01 <federico.bartolomei3@studio.unibo.it>
2014-11-16 15:46:30 +0000

    added fraction and whole recognition routines and more tests

--- FractionCalculator.java
+++ FractionCalculator.java
@@ -18,7 +18,10 @@ public class FractionCalculator {
 	
 	public Fraction evaluate(Fraction fraction, String inputString) {
 			String[] input = inputString.split("\\s+");	// matches one or more whitespaces
-			int i=0;
+			int i=0;	// position of an element of String[] input
+			int j=0;	// position of a char in a specific element of String[] input
+			int numerator = 0;
+			int denominator = 0;
 			for(; i<input.length; i++) {	
 				System.out.println("input at position " + i + " = " + input[i]);	// just for testing
 				if(input[i].equals("+") || input[i].equals("-") 
@@ -28,8 +31,30 @@ public class FractionCalculator {
 						reset();
 						return fraction;
 					}
+				} else { 
+					w: for(j=1; j<input[i].length(); j++) { // look for a whole number
+						if (input[i].charAt(0) != '-'
+						&& !Character.isDigit(input[i].charAt(0))) {
+							break w;
+						} else {
+							if(!Character.isDigit(input[i].charAt(j))) {
+								break w;
+							} else {
+								numerator = Integer.parseInt(input[i]);
+								denominator = 1;
+							}
+						}
+					}						
+					for(; j<input[i].length(); j++) {			// look for a fraction
+						if(input[i].charAt(j) == '/') {
+							numerator = Integer.parseInt(input[i].substring(0,j));
+							denominator = Integer.parseInt(input[i].substring(j+1));
+						}
+					}
 				}
-			}
+				System.out.println("numerator = " + numerator);	
+				System.out.println("denominator = " + denominator);
+				}						
 			return fraction;
 	}
 	
--- FractionCalculatorTest.java
+++ FractionCalculatorTest.java
@@ -5,8 +5,8 @@ public class FractionCalculatorTest {
 		
 		FractionCalculator c = new FractionCalculator();
 		System.out.println("Remembered operation at start: " + c.getRememberedOperation());
-		System.out.println("TEST 1 (evaluate input '1/2 / 	')");
-		c.evaluate(c.getStoredValue(), "1/2 /		");	// test 1
+		System.out.println("TEST 1 (evaluate input '1/2 / -499 	')");
+		c.evaluate(c.getStoredValue(), "1/2 / -499 	");	// test 1
 		System.out.println("operation after test1 (should be /): " + c.getRememberedOperation() + '\n');
 		System.out.println("TEST 2 (evaluate input '0/2')");
 		c.evaluate(c.getStoredValue(), "0/2");	// test 2
@@ -15,6 +15,7 @@ public class FractionCalculatorTest {
 		c.evaluate(c.getStoredValue(), "1/2 - * 28/13");	// test 3
 		System.out.println("operation after test3 (should be null): " + c.getRememberedOperation() + '\n');
 		System.out.println("TEST 4 (evaluate input '	 12 3/-5 1/2 * 	3/4  	')");
+		// '\t' get stored as element [0], should tackle that
 		c.evaluate(c.getStoredValue(), "	 12 3/-5 1/2 * 	3/4  	");	// test 4
 		System.out.println("operation after test4 (should be *): " + c.getRememberedOperation() + '\n');	
 	
