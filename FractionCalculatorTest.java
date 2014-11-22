aa1622dfcf1b7651d1ae460a8f4d776232dfe8b3
fbarto01 <federico.bartolomei3@studio.unibo.it>
2014-11-22 19:29:02 +0000

    tests edited

--- FractionCalculatorTest.java
+++ FractionCalculatorTest.java
@@ -5,32 +5,32 @@ public class FractionCalculatorTest {
 		
 		FractionCalculator c = new FractionCalculator();
 		System.out.println();
-		printInitialValues(c);
-		System.out.println("TEST EXAMPLE (evaluate input ' 1/2 - 3/4 * abs')");
-		System.out.println(c.evaluate(c.getStoredValue(), " 1/2 - 3/4 * abs").toString());	// test 0
-		System.out.println("Stored value after test1 (should be 1/4): " + c.getStoredValue().toString() + '\n');
+		System.out.println("INITIAL VALUES:");
+		printValues(c);
+		System.out.println("TEST EXAMPLE (evaluate input ' 1/2 - 3/4 * abs	')");
+		c.evaluate(c.getStoredValue(), " 1/2 - 3/4 * abs	").toString();	// test Example - in specs
+		printValues(c);
+		System.out.println("TEST EXAMPLE (CONTINUE) (evaluate input '	8 7/8  neg   +')");
+		c.evaluate(c.getStoredValue(), "	8 7/8  neg   +").toString();	// test Example (continue) - in specs
+		printValues(c);
 		System.out.println("TEST 1 (evaluate input '2/2 + 4/2)");
-		System.out.println(c.evaluate(c.getStoredValue(), "2/2 + 4/2").toString());	// test 1
-		System.out.println("Stored value after test1 (should be -3): " + c.getStoredValue().toString() + '\n');
-		System.out.println("TEST 2 (evaluate input ' a')");
-		printInitialValues(c);
-		c.evaluate(c.getStoredValue(), " a");	// test 2
-		System.out.println("Stored value after test2 (should be 3): " + c.getStoredValue().toString() + '\n');
+		c.evaluate(c.getStoredValue(), "2/2 + 4/2").toString();	// test 1
+		printValues(c);
+		System.out.println("TEST 2 (evaluate input ' N')");
+		c.evaluate(c.getStoredValue(), " N");	// test 2
+		printValues(c);
 		System.out.println("TEST 3 (evaluate input '1/2 - -28/13')");
-		printInitialValues(c);
 		c.evaluate(c.getStoredValue(), "1/2 - -28/13");	// test 3
-		System.out.println("Stored value after test3 (should be 69/26): " + c.getStoredValue().toString() + '\n');
+		printValues(c);
 		System.out.println("TEST 4 (evaluate input '	12 3/-5 1/2 * 	8/-4  	/')");
-		printInitialValues(c);
 		c.evaluate(c.getStoredValue(), "	12 3/-5 1/2 * 	8/-4  	/");	// test 4
-		System.out.println("Stored value after test3 (should be -1): " + c.getStoredValue().toString() + '\n');
+		printValues(c);
 	
 	}
 	
-	public static void printInitialValues(FractionCalculator a) {
-		System.out.println("Initial value: " + a.getStoredValue().toString());
-		System.out.println("Remembered operation at start (should be null): " + a.getRememberedOperation() + '\n');
-
+	public static void printValues(FractionCalculator a) {
+		System.out.println("Stored value: " + a.getStoredValue().toString());
+		System.out.println("Remembered operation: " + a.getRememberedOperation() + '\n');
 	}
 	
 }
